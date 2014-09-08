b38244481dc754822d8abab2b9352095f7d8393d
Peter Armstrong <peter@xap.ie>
2014-09-08 10:10:51 +0100

    photo test compete

--- spec/contact_photo_spec.rb
+++ spec/contact_photo_spec.rb
@@ -1,5 +1,6 @@
 require 'onepageapi'
 require 'json_spec'
+require 'base64'
 
 api_login = 'peter+apitest@xap.ie' # put your login details here
 api_pass = 'devteam apitest 5' # put your password here
@@ -7,9 +8,45 @@ samples = OnePageAPISamples.new(api_login, api_pass)
 samples.login
 
 describe 'Contact Photo' do
+  before(:each) do
+    new_contact_details = ({
+      'first_name' => 'Photo',
+      'last_name' => 'Contact'
+    })
 
-  it 'should upload the photo url' do
+    new_contact = samples.create_contact(new_contact_details)
+    @new_contact_id = new_contact['contact']['id']
+  end
 
+  after(:each) do
+    samples.delete("contacts/#{@new_contact_id}.json")
+  end
+ 
+  it 'should upload a jpg photo' do
+    image = File.open('testimages/mic.jpg', 'r')
+    base64_image = Base64.encode64(image.read)
+    response = samples.put("contacts/#{@new_contact_id}/contact_photo.json", 'image' => base64_image)
+    expect(response['status']).to be 0
+    expect(response['data']['contact']['id']).to eq @new_contact_id
+    expect(response['data']['contact']['photo_url']).not_to be nil
+  end
+
+  it 'should upload a png photo' do
+    image = File.open('testimages/mic.png', 'r')
+    base64_image = Base64.encode64(image.read)
+    response = samples.put("contacts/#{@new_contact_id}/contact_photo.json", 'image' => base64_image)
+    expect(response['status']).to be 0
+    expect(response['data']['contact']['id']).to eq @new_contact_id
+    expect(response['data']['contact']['photo_url']).not_to be nil
+  end
+
+  it 'should upload a png pretending to be a jpg' do
+    image = File.open('testimages/png_type.jpg', 'r')
+    base64_image = Base64.encode64(image.read)
+    response = samples.put("contacts/#{@new_contact_id}/contact_photo.json", 'image' => base64_image)
+    expect(response['status']).to be 0
+    expect(response['data']['contact']['id']).to eq @new_contact_id
+    expect(response['data']['contact']['photo_url']).not_to be nil
   end
 
 end
