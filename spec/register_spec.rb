b08d0027d4da41a668571cc01432d1ebc9078d2b
Peter Armstrong <peter@xap.ie>
2014-10-21 16:49:12 +0100

    updated tests

---
+++ spec/register_spec.rb
@@ -0,0 +1,24 @@
+require 'onepageapi'
+require 'json_spec'
+
+samples = OnePageAPISamples.new
+
+describe 'Registration test' pending: true do
+
+  it 'should register the user', pending: true do
+    params = {
+      email: 'peter+newuser2@xap.ie',
+      password: 'newuser',
+      plan: 'growing',
+      firstname: 'Peter',
+      lastname: 'arms',
+      terms: true
+    }
+
+    response = samples.post('register.json', params)
+
+    puts response
+    
+    
+  end
+end
--- spec/tags_spec.rb
+++ spec/tags_spec.rb
@@ -5,18 +5,31 @@ samples = OnePageAPISamples.new
 samples.login
 
 describe 'Get tags' do
-  it 'should not have action_stream_count parameter' do
-    response = samples.get('tags.json')
-    expect(response['data']['tags'][0].to_json).not_to have_json_path('action_stream_count')
-  end
+  # it 'should not have action_stream_count parameter' do
+  #   response = samples.get('tags.json')
+  #   expect(response['data']['tags'][0].to_json).not_to have_json_path('action_stream_count')
+  # end
 
-  it 'call with action_stream_count=1 param should return action_stream_count parameter' do
-    response = samples.get('tags.json?action_stream_count=1')
-    expect(response['data']['tags'][0].to_json).to have_json_path('action_stream_count')
-  end
+  # it 'call with action_stream_count=1 param should return action_stream_count parameter' do
+  #   response = samples.get('tags.json?action_stream_count=1')
+  #   expect(response['data']['tags'][0].to_json).to have_json_path('action_stream_count')
+  # end
 
-  it 'call with action_stream_count=true param should return action_stream_count parameter' do
-    response = samples.get('tags.json?action_stream_count=true')
-    expect(response['data']['tags'][0].to_json).to have_json_path('action_stream_count')
+  # it 'call with action_stream_count=true param should return action_stream_count parameter' do
+  #   response = samples.get('tags.json?action_stream_count=true')
+  #   expect(response['data']['tags'][0].to_json).to have_json_path('action_stream_count')
+  # end
+
+  # it 'should create a new tag with tags.json endpoint' do
+  #   new_tag_name = rand(36**8).to_s(36)
+  #   response = samples.post('tags.json', 'name' => new_tag_name)
+  #   expect(response['data']['name']).to eq new_tag_name
+  # end
+
+  it 'should create a new tag with tags/{name}.json endpoint' do
+    new_tag_name = rand(36**8).to_s(36)
+    response = samples.post("tags/#{new_tag_name}.json", 'bo' => 'sdf')
+    puts response
+    expect(response['data']['name']).to eq new_tag_name
   end
 end
